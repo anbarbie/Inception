0d014ded265f87927c718428b8e281e9d01cf818
anbarbie <siliose@hotmail.fr>
2022-12-07 13:59:48 +0100

    fixed

--- srcs/requirements/mariadb/conf/entrypoint.sh
+++ srcs/requirements/mariadb/conf/entrypoint.sh
@@ -4,9 +4,9 @@
 
 service mysql start
 
-echo "CREATE USER IF NOT EXISTS antbarbi@localhost IDENTIFIED BY 'password';" | mysql -u root
+echo "CREATE USER IF NOT EXISTS 'antbarbi'@'localhost' IDENTIFIED BY 'password';" | mysql -u root
 echo "CREATE DATABASE IF NOT EXISTS db1;" | mysql -u root
-echo "GRAND ALL PRIVILEGES *.* for antbarbi@'%' IDENTIFIED BY 'password';" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON *.* TO 'antbarbi'@'localhost' IDENTIFIED BY 'password';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
 
 exec mysqld -u root
