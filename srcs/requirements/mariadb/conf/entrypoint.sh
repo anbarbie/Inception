d452d9135f2cfb79f04eaa531e6e8842d99f0d88
anbarbie <siliose@hotmail.fr>
2022-12-11 18:02:39 +0100

    compose

--- srcs/requirements/mariadb/conf/entrypoint.sh
+++ srcs/requirements/mariadb/conf/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-#mysql_install_db --defaults-file=/etc/mysql/my.cnf
+#mysql_install_db --defaults-file=/etc/mysql/my.cnf --user=mysql
 
 service mysql start
 
-echo "CREATE USER IF NOT EXISTS 'antbarbi'@'localhost' IDENTIFIED BY 'password';" | mysql -u root
-echo "CREATE DATABASE IF NOT EXISTS db1;" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON *.* TO 'antbarbi'@'%' IDENTIFIED BY 'password';" | mysql -u root
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u root
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABSE;" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
 
 exec mysqld -u root
