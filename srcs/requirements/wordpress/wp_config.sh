568f0b684bff0a9786cbf9bdf3b46ad5cac0c906
anbarbie <siliose@hotmail.com>
2023-01-19 12:02:37 +0100

    healthcheck + proper redirect

--- srcs/requirements/wordpress/wp_config.sh
+++ srcs/requirements/wordpress/wp_config.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-sleep 10
 wp config create --path=/var/www/html/wordpress --allow-root \
 	--dbname=$MYSQL_DATABASE \
 	--dbuser=$MYSQL_USER \
@@ -8,12 +7,12 @@ wp config create --path=/var/www/html/wordpress --allow-root \
 	--dbhost=$WP_DB_HOST
 
 wp core install --allow-root --path=/var/www/html/wordpress \
-	--url=https://localhost/wordpress \
+	--url=https://localhost/ \
 	--title=Inception \
 	--admin_user=$WP_ADMIN_NAME \
 	--admin_password=$WP_ADMIN_PASSWORD \
 	--admin_email=$WP_ADMIN_EMAIL \
-	--skip-email	
+	--skip-email
 
 wp user create --allow-root --path=/var/www/html/wordpress $WP_USER_NAME $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD
 
